384ee85a40b05f8fff0002001cd79561ec65c991
Ahmet Omak <ahmetaltioglu@hotmail.com>
2022-05-19 15:48:27 +0300

    feat(test): add tests

--- Assets/SimpleJSON/Tests/EditMode/SimpleJsonTests.cs
+++ Assets/SimpleJSON/Tests/EditMode/SimpleJsonTests.cs
@@ -1,8 +1,23 @@
-using UnityEngine.TestTools;
-using System.Collections;
 using NUnit.Framework;
+using SimpleJSON;
 
 public class SimpleJsonTests
 {
-        
+    [Test]
+    public void SaveLoadTest()
+    {
+        var testSaveData = new TestSaveData(10);
+
+        SaveManager.Save(testSaveData, SavePath.GAMESAVEPATH);
+
+        Assert.AreEqual(10, SaveManager.Load<TestSaveData>(SavePath.GAMESAVEPATH).levelCount);
+    }
+
+    [Test]
+    public void ResetTest()
+    {
+        SaveManager.Reset<TestSaveData>(SavePath.GAMESAVEPATH, new TestSaveData(5));
+
+        Assert.AreEqual(5, SaveManager.Load<TestSaveData>(SavePath.GAMESAVEPATH).levelCount);
+    }
 }
